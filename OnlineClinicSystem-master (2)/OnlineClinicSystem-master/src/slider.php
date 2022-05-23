c6e035d26148588d1e761700bc62ef8ee3855498
Vaishali-marsone <vaishalimarsone@gmail.com>
2022-05-23 22:37:55 +0530

    File uploads

---
+++ OnlineClinicSystem-master (2)/OnlineClinicSystem-master/src/slider.php
@@ -0,0 +1,74 @@
+<section id="sliderArea">
+    <!-- Start slider wrapper -->
+    <div class="top-slider">
+        <!-- Start First slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img alt="" src="images/14.jpg">
+            </div>
+            <div class="slider-text">
+                <h2>An <strong>Excellent Source</strong> for your Health CheckUps.</h2>
+                <p><strong>SPA Pathcare</strong> is suitable for any health and medical related queries or appointments
+                </p>
+                <div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>
+            </div>
+        </div>
+        <!-- End First slide -->
+
+        <!-- Start 2nd slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img alt="" src="images/15.jpg">
+            </div>
+            <div class="slider-text">
+                
+                                <!--<div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>-->
+            </div>
+        </div>
+        <!-- End 2nd slide -->
+
+        <!-- Start Third slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img alt="" src="images/7.jpg">
+            </div>
+            <div class="slider-text">
+                                <!--<div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>-->
+            </div>
+        </div>
+        <!-- End Third slide -->
+
+        <!-- Start Fourth slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img alt="" src="images/12.jpg">
+            </div>
+            <div class="slider-text">
+                <!--<div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>-->
+            </div>
+        </div>
+        <!-- End Fourth slide -->
+
+        <!-- Start Fifth slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img alt="" src="images/9.jpg">
+            </div>
+            <div class="slider-text">
+                
+                <!--<div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>-->
+            </div>
+        </div>
+        <!-- End Fifth slide -->
+    </div><!-- /top-slider -->
+</section>
